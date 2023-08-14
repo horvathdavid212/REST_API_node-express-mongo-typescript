b4356efe8ccd2bfb68134ff7c6fef8bbebd05280
Horváth Dávid <horvath.david212@gmail.com>
2023-08-14 16:15:01 +0200

    a3

--- db/MONGO_config.ts
+++ db/MONGO_config.ts
@@ -1,6 +1,6 @@
 const config = {
   MONGO_URL:
-    "mongodb+srv://user:pass@cluster1.gbiybcy.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://horvathdavid212:v0dpZq94cTwPGF8Z@cluster1.gbiybcy.mongodb.net/?retryWrites=true&w=majority",
 };
 
 export default config;
